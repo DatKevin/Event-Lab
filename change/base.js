e634b8de89bbbd69d92ec7ee821a74fd402fc42b
Kevin Dat Nguyen <datkevin@gmail.com>
2019-10-30 16:29:15 -0700

    Finished change and bonus challenge

--- change/base.js
+++ change/base.js
@@ -2,3 +2,34 @@ console.log("Sanity Check: JS is working!");
 
 let element = document.querySelector("#time")
 element.textContent = Date.now()
+
+let button = document.createElement("button")
+button.innerText = "Add them!"
+
+let button2 = document.createElement("button")
+button2.innerText = "Reset"
+
+let parentBody = document.querySelector("body")
+
+parentBody.append(button)
+parentBody.append(button2)
+
+let addthem = function() {
+	let x = document.querySelector("#left")
+	let y = document.querySelector("#right")
+	let z = Number(x.value) + Number(y.value)
+	let total = document.querySelector("#total")
+	total.value = z
+}
+
+let clear = function() {
+	let x = document.querySelector("#left")
+	let y = document.querySelector("#right")
+	let total = document.querySelector("#total")
+	total.value = ""
+	x.value = ""
+	y.value = ""
+}
+
+button.addEventListener("click", addthem)
+button2.addEventListener("click", clear)
